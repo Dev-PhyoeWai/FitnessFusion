c7886d29169aa7187a07c0c1b9c61fa15ef5380c
Kaung set Paing <kaungsetpaing@Kaungs-MacBook-Pro.local>
2024-07-12 11:09:28 +0630

    delete workout & workoutplan

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\SubscriptionTypeController;
 use App\Http\Controllers\SubWeightController;
 use App\Http\Controllers\WeightTypeController;
 use App\Http\Controllers\WorkoutController;
+use App\Http\Controllers\WorkoutPlanController;
 use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Route;
 
@@ -47,6 +48,6 @@ Route::resource('sub-weights', SubWeightController::class);
 Route::apiResource('subscription-types', SubscriptionTypeController::class);
 Route::apiResource('weight-types', WeightTypeController::class);
 
-Route::resource('workout',WorkoutController::class);
+
 
 require __DIR__.'/auth.php';
