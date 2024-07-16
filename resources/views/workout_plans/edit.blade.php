3b698960850c05f52f14a9ed32b43ed1bfaea989
Kaung set Paing <kaungsetpaing@Kaungs-MacBook-Pro.local>
2024-07-16 14:50:21 +0630

    edit

--- resources/views/workout_plans/edit.blade.php
+++ resources/views/workout_plans/edit.blade.php
@@ -29,7 +29,9 @@
             <div class="mb-4">
                 <label class="block text-gray-700">Image</label>
                 <input type="file" name="image"  class="w-full border-gray-300 rounded">
-                
+                @if($workoutPlan->image)
+                <img src="{{ asset($workoutPlan->image) }}" alt="Current Image" width="100">
+            @endif
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700">Gender</label>
